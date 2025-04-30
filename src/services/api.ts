7d79226b96a313a9680207317ac81d63472b33a3
Jackson Cheriyan Kollareth <138844080+ChackzWolf@users.noreply.github.com>
2025-04-30 20:13:31 +0530

    Update:changed tab name

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError, AxiosResponse } from 'axios';
 import { toast } from 'react-toastify';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
